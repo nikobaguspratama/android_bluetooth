c8eba0114e6a4eba6d169caa77a5187a51a061b3
candrarestu23 <candra.restu22@gmail.com>
2018-07-16 00:31:25 +0700

    local change

--- app/src/main/java/com/example/nikobaguspratama/myapplication/bluetoothActivity.java
+++ app/src/main/java/com/example/nikobaguspratama/myapplication/bluetoothActivity.java
@@ -44,7 +44,7 @@ public class bluetoothActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bluetooth_activity);
         Intent newIntent = getIntent();
         address = newIntent.getStringExtra(MainActivity.EXTRA_ADDRESS);
         mDatabaseR = FirebaseDatabase.getInstance().getReference().child("data-bluetooth");
